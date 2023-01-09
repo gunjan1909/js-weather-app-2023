856ccb2f78c19ee31a2c9b663dd046a673cf9a4f
gunjan1909 <bhanarkaranita@gmail.com>
2023-01-10 01:06:53 +0530

    .

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
 import "./style.css";
+import { getWeather } from "./weather";
+
+getWeather(10, 10, Intl.DateTimeFormat().resolvedOptions().timeZone).then(
+  (res) => {
+    console.log(res.data);
+  }
+);
---
+++ weather.js
@@ -0,0 +1,12 @@
+//https://api.open-meteo.com/v1/forecast?hourly=temperature_2m,apparent_temperature,precipitation,weathercode,windspeed_10m&daily=weathercode,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,precipitation_sum&current_weather=true&timeformat=unixtime
+
+import axios from "axios";
+
+export function getWeather(lat, lon, timezone) {
+  return axios.get(
+    "https://api.open-meteo.com/v1/forecast?hourly=temperature_2m,apparent_temperature,precipitation,weathercode,windspeed_10m&daily=weathercode,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,precipitation_sum&current_weather=true&timeformat=unixtime",
+    {
+      params: { latitude: lat, longitude: lon, timezone },
+    }
+  );
+}
